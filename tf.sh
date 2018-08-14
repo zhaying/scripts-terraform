7c9db73c07e478c0ea55f099f79950420a083021
Zhaying <8896093+zhaying@users.noreply.github.com>
2018-08-14 07:33:00 -0400

    Updated to grab latest version from github.com

--- tf.sh
+++ tf.sh
@@ -1,3 +1,18 @@
-sudo wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip 
-sudo unzip terraform_0.11.7_linux_amd64.zip -d /usr/local/bin/ 
+#!/bin/bash
+# Variables
+tf_repo=https://releases.hashicorp.com/terraform/
+tf_ver=$(curl -s https://api.github.com/repos/hashicorp/terraform/tags | grep name | head -n 1 | tr -d '",' | awk -F'v' '{print $2}')
+tf_filename=terraform_
+tf_fn_suffix=_linux_amd64.zip
+tf_fn_latest="$tf_filename$tf_ver$tf_fn_suffix"
+tf_url="$tf_repo$tf_ver/$tf_fn_latest"
+#check variablesa
+echo $tf_ver
+echo $tf_url
+echo $tf_fn_latest
+# Download the file
+sudo wget $tf_url
+# Unzip the file
+sudo unzip $tf_fn_latest -d /usr/local/bin/
+# Check to see if it installed
 terraform --version
