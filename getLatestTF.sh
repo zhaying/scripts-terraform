3f894cdd2a0fe8f95e8df4204defdb0154d225fe
Zhaying <8896093+zhaying@users.noreply.github.com>
2020-06-20 03:18:25 -0400

    Excluded non-stable tags from list

--- getLatestTF.sh
+++ getLatestTF.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Expected Url Format as of 8/14/2018
 # https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+
 # Variables
 os="_$(uname -s | awk '{print tolower($0)}')"
 os_arch=$(uname -m)
 tf_repo=https://releases.hashicorp.com/terraform
-tf_ver=$(curl -s https://api.github.com/repos/hashicorp/terraform/tags | grep name | head -n 1 | tr -d '",' | awk -F'v' '{print $2}')
+tf_ver=$(curl -s https://api.github.com/repos/hashicorp/terraform/tags | grep -v 'alpha\|beta\|rc' | grep name | head -n 1 | tr -d '",' | awk -F'v' '{print $2}')
 tf_filename=terraform_
+
 # Determine ARCH
 if [ $os_arch == 'x86_64' ]
 then
@@ -17,10 +19,12 @@ if [ $os_arch == 'i386' ]
 then
   arch="_386"
 fi
+
 # Variables after ARCH determination
 tf_fn_suffix="$os$arch.zip"
 tf_fn_latest="$tf_filename$tf_ver$tf_fn_suffix"
 tf_url="$tf_repo/$tf_ver/$tf_fn_latest"
+
 # Check variables
 echo $os
 echo $os_arch
@@ -34,7 +38,9 @@ echo $tf_url
 
 # Download the file
 sudo wget $tf_url
+
 # Unzip the file
 sudo unzip $tf_fn_latest -d /usr/local/bin/
+
 # Check to see if it installed
 terraform --version
